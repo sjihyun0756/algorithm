e9756b1f221f644724ad44bc416b73d08ab0f94b
jihyun <jihyun_0756@naver.com>
2025-02-14 01:19:38 +0900

    [25-02-14] Update

--- src/etc/IM대비5_봉사활동.java
+++ src/etc/IM대비5_봉사활동.java
@@ -3,21 +3,24 @@ package etc;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class IM대비5_봉사활동 {
-	public static void main(String[] args) throws  IOException {
+	static int result = 1001;
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		
 		for(int tc = 1; tc<=T; tc++) {
-			System.out.println();
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int N = Integer.parseInt(st.nextToken());
 			int min = Integer.parseInt(st.nextToken());
 			int max = Integer.parseInt(st.nextToken());
-			
 			
 			st = new StringTokenizer(br.readLine());
 			int[] score = new int[N];
@@ -29,42 +32,48 @@ public class IM대비5_봉사활동 {
 			int maxScore = score[N-1];
 			
 			int[] cnt = new int[maxScore+1];
-			for(int v : score) cnt[v]++;
-			
-			int d = 0; //서로 다른 점수 개수
-			for(int i=0; i<cnt.length; i++) {
-				if(cnt[i] != 0) d++;
-			}
-			
-			
-			
-			int result = 0;
-			if(d==3) {
-				if(min<cnt[0] && cnt[0]<max && min<cnt[cnt.length-1] && cnt[cnt.length-1]<max) {
-					result = cnt[cnt.length-1]-cnt[0];
-				}else {
-					result = -1;
+			Set<Integer> unique = new LinkedHashSet<>();
+			for(int v : score) {
+				cnt[v]++;
+				if(v != 0) {
+					unique.add(v);
 				}
-			}else {
-				System.out.println("다른 케이스");
 			}
+//			System.out.println("카운팅" +Arrays.toString(cnt));
+			List<Integer> uniqueList = new ArrayList<>(unique);
 			
-//			//일단 누적합 배열 따로 안 만듦
-//			for(int i=1; i<cnt.length; i++) cnt[i] += cnt[i-1];
+//			System.out.println("고유 점수만(인덱스 역할)" +uniqueList);
+			
+			int[] cntSum = new int[cnt.length];
+			for(int i=1; i<cnt.length; i++) {
+				cntSum[i] = cnt[i] + cntSum[i-1];
+			}
+//			System.out.println("누적합" +Arrays.toString(cntSum));
+			
+			
+			for(int i=1; i< uniqueList.size()-1; i++) {
+				//score2 이상 : 우수 분반 / score1 이상 score2 미만 : 보통 분반 / score1 미만 : 부진 분반
+				for(int j=i+1; j<uniqueList.size(); j++) {
+					result = 1001;
+					int score1 = uniqueList.get(i);
+					int score2 = uniqueList.get(j);
+//					System.out.println("score1 +score2::::::::"+score1 + " " +score2);
+					
+					int lowClass = cntSum[score1-1];
+					if(lowClass < min || max < lowClass)  continue;
+					int midClass = cntSum[score2-1]-cntSum[score1-1];
+					if(midClass < min || max < midClass)  continue;
+					int highClass = cntSum[cntSum.length-1]-cntSum[score2-1];
+					if(highClass < min || max < highClass)  continue;
+//					System.out.println("lowClass : " + lowClass + "midClass : " + midClass + "highClass : " + highClass);
+//					System.out.println("highClass-lowClass " + (highClass-lowClass));
+					result = Math.min(Math.abs(highClass-lowClass), result);
+//					System.out.println(" result ::::::" + result);
+				}
+			}
 
-			System.out.println(result);
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
+			if(result ==1001) System.out.println("#"+tc+" "+"-1");
+			else System.out.println("#"+tc+" "+result);
 		} //tc for문
-		
 	}
-
 }
---
+++ src/etc/정렬.java
@@ -0,0 +1,86 @@
+package etc;
+
+import java.util.Arrays;
+
+public class 정렬 {
+
+	static void 카운팅정렬(int arr[]) {
+		int max = 0;
+		for(int v : arr) {
+			max = Math.max(max, v);
+		}
+		
+		int[] cnt = new int[max+1];
+		for(int v : arr) {
+			cnt[v]++;
+		}
+		
+		for(int i=1; i<cnt.length; i++) {
+			cnt[i] += cnt[i-1];
+		}
+		
+		int[] sortArr = new int[arr.length];
+		for(int i=arr.length-1; i>=0; i--) {
+			sortArr[--cnt[arr[i]]] = arr[i];
+		}
+		
+		System.out.println("카운팅정렬"+Arrays.toString(sortArr));
+	}
+
+	static void 버블정렬(int arr[]) {
+		
+		for(int i=0; i<arr.length-1; i++) {
+			for(int j=0; j<arr.length-i-1; j++) {
+				if(arr[j]>arr[j+1]) {
+					int tmp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = tmp;
+				}
+			}
+		}
+		System.out.println("버블정렬"+Arrays.toString(arr));
+	}
+
+	static void 선택정렬(int arr[]) {
+		
+		for(int i=0; i<arr.length; i++) {
+			int minIdx = i;
+			for(int j=i+1; j<arr.length; j++) {
+				if(arr[minIdx] > arr[j]) {
+					minIdx = j;
+				}
+			}
+			int tmp = arr[minIdx];
+			arr[minIdx] = arr[i];
+			arr[i] = tmp;
+		}
+		System.out.println("선택정렬"+Arrays.toString(arr));
+	}
+
+	static void 이진탐색(int arr[], int key) {
+		
+		int left = 0;
+		int right = arr.length-1;
+		while(left<right) {
+			int mid = (left+right)/2;
+			if(arr[mid]==key) {
+				System.out.println(mid+"번째 인덱스에 위치함");
+				break;
+			}else if(arr[mid] > key) {
+				right = mid-1;
+			}else {
+				left = mid +1;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		int[] arr = new int[]{3,7,1,5,2,8,3,1,5,5,2,10,9,0,3};
+		
+		카운팅정렬(arr);
+		버블정렬(arr);
+		선택정렬(arr);
+		이진탐색(new int[]{1,2,3,4,5,6,7,8,9,10},8);
+	}
+}
