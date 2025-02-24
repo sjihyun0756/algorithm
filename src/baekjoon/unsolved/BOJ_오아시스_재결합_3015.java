8999cec14bcdf11ed560aef921a198426b52cfb0
sjihyun <sjihyun0756@gmail.com>
2025-02-25 00:08:24 +0900

    [25-02-25] Update

--- src/baekjoon/unsolved/BOJ_오아시스_재결합_3015.java
+++ src/baekjoon/unsolved/BOJ_오아시스_재결합_3015.java
@@ -1,5 +1,43 @@
 package baekjoon.unsolved;
 
-public class BOJ_오아시스_재결합_3015 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
+public class BOJ_오아시스_재결합_3015 {
+	public static void main(String[] args) throws  IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Stack<Integer> s = new Stack<>();
+		int N = Integer.parseInt(br.readLine());
+		
+		int cnt = 0; 
+		int first = Integer.parseInt(br.readLine());
+		int second = Integer.parseInt(br.readLine());
+		if(first<=second) s.push(second);
+		else {
+			s.push(first);
+			s.push(second);
+		}//2개 미리 넣어놓기
+		cnt++;
+		
+		for(int i=2; i<N; i++) {
+			int num = Integer.parseInt(br.readLine());
+			if(s.isEmpty()) s.push(num);
+			else if(s.peek() < num) { //입력값이 더 큼
+				cnt += s.size();
+				while(s.size() != 1) {
+					s.pop();
+				}
+				s.push(num);
+			}else if(s.peek() == num) {
+				cnt += s.size();
+				s.push(num);
+			}else if(s.peek() > num) { //입력값이 더 작음
+				s.push(num);
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
+	}
 }
