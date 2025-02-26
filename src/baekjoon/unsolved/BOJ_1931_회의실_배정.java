5d9615fabf76a40cf3736f1a38a91df31047fde6
sjihyun <sjihyun0756@gmail.com>
2025-02-26 21:51:43 +0900

    [25-02-26] Update

--- src/baekjoon/unsolved/BOJ_1931_회의실_배정.java
+++ src/baekjoon/unsolved/BOJ_1931_회의실_배정.java
@@ -1,5 +1,16 @@
 package baekjoon.unsolved;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class BOJ_1931_회의실_배정 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		
+	}
 
 }
