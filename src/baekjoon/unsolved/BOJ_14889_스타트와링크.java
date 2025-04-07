1e7c4fd2f135bc45f9842c0d8cbcae8d77a0844f
sjihyun <sjihyun0756@gmail.com>
2025-04-08 00:38:12 +0900

    [25-04-08] Update

--- src/baekjoon/unsolved/BOJ_14889_스타트와링크.java
+++ src/baekjoon/unsolved/BOJ_14889_스타트와링크.java
@@ -4,22 +4,39 @@ import java.io.*;
 import java.util.*;
 
 public class BOJ_14889_스타트와링크 {
-	static int N;
+	static int N, minSynergy;
 	static int[][] map;
 	static int[] result;
 	
 	private static void findSet(int depth, int last) {
-		if(depth == N/2 && result[0] == 1) {
-			int cal1 = 0, cal2 = 0;
-			
-			
+		if(depth == N/2) {
+			calculate();
 			return;
 		}
 		
 		for(int i=last+1; i<N; i++) {
-			result[depth] = i+1;
+			result[depth] = i;
 			findSet(depth+1, i);
 		}
+	}
+	
+	private static void calculate() {
+		boolean[] isSelected = new boolean[N];
+		for(int n : result) {
+			isSelected[n] = true;
+		}
+		
+		int synergy1 = 0, synergy2 = 0;
+		for (int i = 0; i < N - 1; i++) {
+			for (int j = i + 1; j < N; j++) {
+				if (isSelected[i] && isSelected[j]) {
+					synergy1 += map[i][j] + map[j][i]; 
+				} else if (!isSelected[i] && !isSelected[j]) {
+					synergy2 += map[i][j] + map[j][i];
+				}
+			}
+		}
+		minSynergy = Math.min(Math.abs(synergy1 - synergy2), minSynergy);
 	}
 	
 	public static void main(String[] args) throws IOException {
@@ -28,6 +45,7 @@ public class BOJ_14889_스타트와링크 {
 		StringTokenizer st;
 		map = new int[N][N];
 		result = new int[N/2];
+		minSynergy = Integer.MAX_VALUE;
 		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j=0; j<N; j++) {
@@ -35,5 +53,6 @@ public class BOJ_14889_스타트와링크 {
 			}
 		}
 		findSet(0, -1);
+		System.out.println(minSynergy);
 	}
 }
---
+++ src/baekjoon/unsolved/BOJ_9663_N_Queen.java
@@ -0,0 +1,14 @@
+package baekjoon.unsolved;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_9663_N_Queen {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		
+		
+	}
+}
