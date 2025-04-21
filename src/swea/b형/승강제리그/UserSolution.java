a74a33bc864d24688afa4882d0dac7908dc9bca5
sjihyun <sjihyun0756@gmail.com>
2025-04-21 14:11:18 +0900

    [25-04-21] Update

---
+++ src/swea/b형/승강제리그/UserSolution.java
@@ -0,0 +1,16 @@
+package swea.b형.승강제리그;
+
+class UserSolution {
+
+    void init(int N, int L, int mAbility[]) {
+    }
+
+    int move() {
+        return 0;
+    }
+
+    int trade() {
+        return 0;
+    }
+
+}
