970dc7c84af82f15a583664a01fd8a075d788d2d
sjihyun <sjihyun0756@gmail.com>
2025-04-22 17:28:26 +0900

    [25-04-22] Update

---
+++ src/baekjoon/unsolved/BOJ_11723_집합.java
@@ -0,0 +1,47 @@
+package baekjoon.unsolved;
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ_11723_집합 {
+	
+	private static StringBuilder sb = new StringBuilder();
+	private static int standardNum = 0;
+	
+	private static StringBuilder operate(String action, int num) {
+		int actionNum = (1<<num);
+		if(action.equals("add)")) {
+			standardNum |= actionNum;
+		}else if(action.equals("remove")) {
+			standardNum -= actionNum;
+		}else if(action.equals("check")) {
+			if ((standardNum & actionNum) != 0) sb.append("1").append("\n");
+			else sb.append("0").append("\n");
+		}else if(action.equals("toggle")) {
+			if ((standardNum & actionNum) != 0) standardNum -= actionNum;
+			else standardNum |= actionNum;
+		}else if(action.equals("all")) {
+			standardNum = (1<<21) -1;
+		}else { //action -> empty
+			standardNum = 0; 
+		}
+		return sb;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			String action = st.nextToken();
+			if(action.equals("all") || action.equals("empty")) operate(action, 0);
+			else {
+				int num = Integer.parseInt(st.nextToken());
+				operate(action, num);
+			}
+		}
+		System.out.println(sb.toString());
+	}
+}
--- src/etc/test.java
+++ src/etc/test.java
@@ -1,9 +1,18 @@
 package etc;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class test {
     public static void main(String[] args) {
-        String a = "abc";
-        System.out.println(a.length());
+    	int n = 5;
+    	for (int mask = 0; mask < (1 << n); mask++) {
+    		System.out.println(mask);
+    	    for (int i = 0; i < n; i++) {
+    	        if ((mask & (1 << i)) != 0) {
+    	            System.out.print(i + " "); // i번째 원소가 선택됨
+    	        }
+    	    }
+    	    System.out.println("-".repeat(20));
+    	}
     }
 }
