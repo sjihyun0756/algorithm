02984a65287004464e35b868c4962d98d8d2b8a0
sjihyun <sjihyun0756@gmail.com>
2025-05-24 23:07:11 +0900

    [25-05-24] Update

--- src/baekjoon/r3_gold/BOJ_10026_적록색약.java
+++ src/baekjoon/r3_gold/BOJ_10026_적록색약.java
@@ -1,26 +1,84 @@
 package baekjoon.r3_gold;
 
 import java.io.*;
+import java.util.*;
 
 public class BOJ_10026_적록색약 {
+	private static int N;
+    private static char[][] map;
+    private static boolean[][] visit;
+    private static final int[] dx = {-1, 1, 0, 0};
+    private static final int[] dy = {0, 0, -1, 1};
 	
-	private static char[][] map;
-	
-	private static void RGBlind() {
-		
-		
-	}
-	
+    private static void cntArea(int i, int j, boolean colorBlind) {
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{i, j});
+        visit[i][j] = true;
+        char color = map[i][j];
+
+        while (!q.isEmpty()) {
+            int[] out = q.poll();
+            int x = out[0], y = out[1];
+
+            for (int d = 0; d < 4; d++) {
+                int nx = x + dx[d];
+                int ny = y + dy[d];
+
+                if (0 <= nx && nx < N && 0 <= ny && ny < N && !visit[nx][ny]) {
+                    char nextColor = map[nx][ny];
+
+                    if (!colorBlind) {
+                        if (nextColor == color) {
+                            visit[nx][ny] = true;
+                            q.offer(new int[]{nx, ny});
+                        }
+                    } else {
+                        if ((color == 'B' && nextColor == 'B') || (color != 'B' && nextColor != 'B')) {
+                            visit[nx][ny] = true;
+                            q.offer(new int[]{nx, ny});
+                        }
+                    }
+                }
+            }
+        }
+    }
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+		
+		N = Integer.parseInt(br.readLine());
 		map = new char[N][N];
+		
 		for(int i=0; i<N; i++) {
 			String input = br.readLine();
 			for(int j=0; j<N; j++) {
 				map[i][j] = input.charAt(j);
 			}
 		}
-		RGBlind();
+		
+		visit = new boolean[N][N];
+        int normalCnt = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visit[i][j]) {
+                	cntArea(i, j, false); 
+                	normalCnt++;
+                }
+            }
+        }
+
+        visit = new boolean[N][N]; 
+        int colorBlindCnt = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visit[i][j]) {
+                	cntArea(i, j, true); 
+                	colorBlindCnt++;
+                }
+            }
+        }
+        
+        System.out.println(normalCnt);
+        System.out.println(colorBlindCnt);
 	}
 }
---
+++ src/baekjoon/unsolved/BOJ_2565_전깃줄.java
@@ -0,0 +1,34 @@
+package baekjoon.unsolved;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BOJ_2565_전깃줄 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		int[][] wires = new int[N][2];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			wires[i][0] = Integer.parseInt(st.nextToken()); 
+			wires[i][1] = Integer.parseInt(st.nextToken()); 
+		}
+		Arrays.sort(wires, (e1, e2) -> Integer.compare(e1[0], e2[0]));
+
+		int[] dp = new int[N];
+		int result = 0;
+		for (int i = 0; i < N; i++) {
+			dp[i] = 1;
+			for (int j = 0; j < i; j++) {
+				if (wires[i][1] > wires[j][1]) {
+					dp[i] = Math.max(dp[i], dp[j] + 1);
+				}
+			}
+			result = Math.max(result, dp[i]);
+		}
+		System.out.println(N - result);
+	}
+}
