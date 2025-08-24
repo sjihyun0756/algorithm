06934bd4b0a7fd1d47def6fb41c5c207221b4fa7
sjihyun <sjihyun0756@gmail.com>
2025-08-24 15:15:20 +0900

    [25-08-24] BOJ 1915 : solved

---
+++ src/baekjoon/r3_gold/BOJ_1915_가장_큰_정사각형.java
@@ -0,0 +1,31 @@
+package baekjoon.r3_gold;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_1915_가장_큰_정사각형 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int[][] map = new int[N+1][M+1];
+		for(int i=1; i<=N; i++) {
+			String input = br.readLine();
+			for(int j=1; j<=M; j++) {
+				map[i][j] = input.charAt(j-1) -'0';
+			}
+		}
+		
+		int[][] dp = new int[N+1][M+1];
+		int max = 0;
+		for(int i=1; i<=N; i++) {
+			for(int j=1; j<=M; j++) {
+				if(map[i][j] != 0) dp[i][j] = Math.min(Math.min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) +1;
+				max = Math.max(max, dp[i][j]);
+			}
+		}
+		System.out.println(max * max);
+	}
+}
